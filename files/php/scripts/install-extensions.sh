7db876510ee2c5940101ac1a48128b1ddd413734
Robbert Müller <robbert.muller@dealerdirect.nl>
2017-06-06 13:53:11 +0200

    improvbe extension installation
    * fixes strange travis-ci build error on ext '-j2'
    * conform to bash style guide

--- files/php/scripts/install-extensions.sh
+++ files/php/scripts/install-extensions.sh
@@ -4,10 +4,10 @@ set -o pipefail # Return exit status of the last command in the pipe that failed
 
 if [ ! -z "$INSTALL_PHP_EXT" ]; then
   for ext in $INSTALL_PHP_EXT; do
-    if [[ ${ext} == 'gd' ]] ; then
+    if [[ "${ext}" = 'gd' ]] ; then
       docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
     fi
-    docker-php-ext-install -j$(nproc) "$ext"
+    docker-php-ext-install "$ext"
   done
 fi
 
