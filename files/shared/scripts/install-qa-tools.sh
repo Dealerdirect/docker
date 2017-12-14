3f0c053b17e9090f4d00938b75788b8b951c4a77
Robbert Müller <robbert.muller@dealerdirect.nl>
2017-12-14 09:35:05 +0100

    hotfixing composer dependency resolver issue

--- files/shared/scripts/install-qa-tools.sh
+++ files/shared/scripts/install-qa-tools.sh
@@ -2,6 +2,7 @@
 set -o errexit  # Exit script when a command exits with non-zero status
 set -o pipefail # Return exit status of the last command in the pipe that failed
 
+composer global require elfet/pure
 curl -O https://raw.githubusercontent.com/DealerDirect/php-qa-tools/master/bin/install.sh
 bash install.sh
 rm -f install.sh
