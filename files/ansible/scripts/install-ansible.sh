61f7d3f3dead47c9071fd41612cb203ebc304361
Robbert Müller <robbert.muller@dealerdirect.com>
2018-10-31 16:35:40 +0100

    Added support for pip3

--- files/ansible/scripts/install-ansible.sh
+++ files/ansible/scripts/install-ansible.sh
@@ -2,13 +2,15 @@
 set -o errexit  # Exit script when a command exits with non-zero status
 set -o pipefail # Return exit status of the last command in the pipe that failed
 
-pip install --upgrade setuptools
-pip install cryptography boto passlib dnspython
+pip=$(which pip) || pip=$(which pip3)
+
+$pip install --upgrade setuptools
+$pip install cryptography boto passlib dnspython
 
 if [ ! -z "$ANSIBLE_VERSION" ]; then
-  pip install "ansible==$ANSIBLE_VERSION" ansible-lint
+  $pip install "ansible==$ANSIBLE_VERSION" ansible-lint
 else
-  pip install ansible ansible-lint
+  $pip install ansible ansible-lint
 fi
 
 rm -fr /sbin/initctl
--- files/shared/scripts/install-pip.sh
+++ files/shared/scripts/install-pip.sh
@@ -2,8 +2,10 @@
 set -o errexit  # Exit script when a command exits with non-zero status
 set -o pipefail # Return exit status of the last command in the pipe that failed
 
-pip install --upgrade setuptools
+pip=$(which pip) || pip=$(which pip3)
+
+$pip install --upgrade setuptools
 
 if [ ! -z "$INSTALL_PIP" ]; then
-  pip install $INSTALL_PIP
+  $pip install $INSTALL_PIP
 fi
