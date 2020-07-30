fc52d2839fc11688d61f246cbf44b1e8b3d7fa10
Robbert Müller <robbert.muller@dealerdirect.com>
2020-07-30 10:26:06 +0200

    Increase memory limit for composer

--- files/shared/scripts/install-qa-tools.sh
+++ files/shared/scripts/install-qa-tools.sh
@@ -3,5 +3,6 @@ set -o errexit  # Exit script when a command exits with non-zero status
 set -o pipefail # Return exit status of the last command in the pipe that failed
 
 curl -O https://raw.githubusercontent.com/DealerDirect/php-qa-tools/master/bin/install.sh
+export COMPOSER_MEMORY_LIMIT=-1
 bash install.sh
 rm -f install.sh
