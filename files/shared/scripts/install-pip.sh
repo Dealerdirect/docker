66f7f465371fb6d76d30f809611c7c4af02073f8
Robbert Müller <robbert.muller@dealerdirect.com>
2020-07-30 11:10:23 +0200

    Prefer python3

--- files/shared/scripts/install-pip.sh
+++ files/shared/scripts/install-pip.sh
@@ -2,7 +2,7 @@
 set -o errexit  # Exit script when a command exits with non-zero status
 set -o pipefail # Return exit status of the last command in the pipe that failed
 
-pip=$(which pip) || pip=$(which pip3)
+pip=$(which pip3) || pip=$(which pip)
 
 $pip install --upgrade setuptools
 
