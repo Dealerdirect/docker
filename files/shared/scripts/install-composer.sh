825ad05ac77e41617f4c2c7c6d91c91571680cde
Robbert Müller <spam.me@grols.ch>
2021-06-10 20:44:38 +0200

    newer qa tools

--- files/shared/scripts/install-composer.sh
+++ files/shared/scripts/install-composer.sh
@@ -2,6 +2,6 @@
 set -o errexit  # Exit script when a command exits with non-zero status
 set -o pipefail # Return exit status of the last command in the pipe that failed
 
-curl -sS https://getcomposer.org/installer | php -- --1
+curl -sS https://getcomposer.org/installer | php -- --2
 mv composer.phar /usr/local/bin/
 ln -s /usr/local/bin/composer.phar /usr/local/bin/composer
--- files/shared/scripts/install-qa-tools.sh
+++ files/shared/scripts/install-qa-tools.sh
@@ -3,6 +3,7 @@ set -o errexit  # Exit script when a command exits with non-zero status
 set -o pipefail # Return exit status of the last command in the pipe that failed
 
 curl -O https://raw.githubusercontent.com/DealerDirect/php-qa-tools/master/bin/install.sh
+sed -e'/prestissimo/d' -i install.sh
 export COMPOSER_MEMORY_LIMIT=-1
 bash install.sh
 rm -f install.sh
